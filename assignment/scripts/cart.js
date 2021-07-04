b479ee8314caf3df4df9cbc460409188351eb826
Christian Fredricks <Christianpfred@gmail.com>
2021-07-04 13:58:13 -0500

    Got adding items and listing items done.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,33 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket=[];
+console.log(`Basket is ${basket}`);
+function addItem (item){
+  if (item >= 0){
+    console.log('That is not a proper item.')
+    return false;
+  }
+  else if (item < 0){
+    console.log('That is not a proper item.')
+    return false;
+  }
+  else if (item = item){
+    basket.push(item);
+    console.log('We have added', basket, 'to the basket.');
+    return true;
+    }
+  }
+addItem('Cheese');
+addItem(4);
+console.log(`Basket is now ${basket}`);
+addItem('Bacon');
+console.log(`Basket is now ${basket}`);
+
+
+function listItems(){
+  for( let i=0; i<basket.length; i++){
+    console.log('Your cart contains', basket[i]);
+  }
+}
+listItems()
