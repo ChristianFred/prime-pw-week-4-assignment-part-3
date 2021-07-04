c30bf25c829aa49ce590ede66c5bafc26891308e
Christian Fredricks <Christianpfred@gmail.com>
2021-07-04 15:19:10 -0500

    I wasn't quite able to get the Max limit of items to work correctly but I think I got everything else working.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,21 +1,19 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const maxItems = 5;
 let basket=[];
 console.log(`Basket is ${basket}`);
 function addItem (item){
   if (item >= 0){
     console.log('That is not a proper item.')
-    return false;
   }
   else if (item < 0){
     console.log('That is not a proper item.')
-    return false;
   }
   else if (item = item){
     basket.push(item);
     console.log('We have added', basket, 'to the basket.');
-    return true;
     }
   }
 addItem('Cheese');
@@ -32,10 +30,38 @@ function listItems(){
 }
 listItems()
 
-function RemoveItems(){
+function Clear(){
   basket.length = 0;
   console.log(basket);
 }
-RemoveItems()
-addItem('Water');
+Clear()
+addItem('Water')
 listItems()
+function isFull(){
+  if (basket.length >= maxItems){
+    console.log('too many items');
+    return true
+  }
+  else if (basket.length < maxItems){
+   console.log('you can keep adding items');
+   return false
+  }
+}
+console.log('Is your cart currently full?', isFull() );
+
+function RemoveItem(item, number){
+  const index = basket.indexOf(number);
+  basket.splice(index, 1);
+  console.log('We have removed an entry from your cart')
+}
+RemoveItem('Water')
+addItem('Pizza')
+addItem('Rockstar')
+addItem('Fire')
+addItem('Cheese Curds')
+addItem('Candy')
+console.log('Is your cart currently full?', isFull() );
+listItems()
+RemoveItem('Fire')
+listItems()
+console.log('Is your cart currently full?', isFull() );
